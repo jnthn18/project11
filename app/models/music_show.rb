35057e010443bef716c48b1d5928f36de259542c
jnthn18 <jnthn18@gmail.com>
2015-04-12 20:22:20 -0400

    Hid the Edit link

--- app/models/music_show.rb
+++ app/models/music_show.rb
@@ -1,6 +1,6 @@
 class MusicShow < ActiveRecord::Base
-	#validates_presence_of to make sure every field has something
-	validates_presence_of :band_name, message: "-- Please enter the band playing on this date."
-	#location should contain city, state
-	validates_format_of :location, with: /.+,\s\w{2}/, message: "-- Please enter the location of show as city, state."
+  #validates_presence_of to make sure every field has something
+  validates_presence_of :band_name, message: "-- Please enter the band playing on this date."
+  #location should contain city, state
+  validates_format_of :location, with: /.+,\s\w{2}/, message: "-- Please enter the location of show as city, state."
 end
